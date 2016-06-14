afe98265df9f32c9218233b4f1f8516be7d214cd
Michael Kuehl <michael@losant.com>
2016-06-14 14:52:38 -0400

    build from latest api

--- lib/losant_rest/client.rb
+++ lib/losant_rest/client.rb
@@ -5,7 +5,7 @@ module LosantRest
   #
   # User API for accessing Losant data
   #
-  # Built For Version 1.3.8
+  # Built For Version 1.3.9
   class Client
     attr_accessor :auth_token, :url
 
@@ -112,7 +112,7 @@ module LosantRest
 
       headers["Accept"]         = "application/json"
       headers["Content-Type"]   = "application/json"
-      headers["Accept-Version"] = "^1.3.8"
+      headers["Accept-Version"] = "^1.3.9"
       headers["Authorization"]  = "Bearer #{self.auth_token}" if self.auth_token
       path = self.url + options.fetch(:path, "")
 
