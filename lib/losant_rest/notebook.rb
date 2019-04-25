4a66a70180a0d907c2e50e1edf06cb8312d00260
Michael Kuehl <michael@losant.com>
2019-04-25 09:48:39 -0400

    remove invalid call

--- lib/losant_rest/notebook.rb
+++ lib/losant_rest/notebook.rb
@@ -318,53 +318,5 @@ module LosantRest
         body: body)
     end
 
-    # Uploads the jupyter notebook file
-    #
-    # Authentication:
-    # The client must be configured with a valid api
-    # access token to call this action. The token
-    # must include at least one of the following scopes:
-    # all.Application, all.Organization, all.User, notebook.*, or notebook.upload.
-    #
-    # Parameters:
-    # *  {string} applicationId - ID associated with the application
-    # *  {string} notebookId - ID associated with the notebook
-    # *  {file} jupyterFile - The jupyter notebook file
-    # *  {string} losantdomain - Domain scope of request (rarely needed)
-    # *  {boolean} _actions - Return resource actions in response
-    # *  {boolean} _links - Return resource link in response
-    # *  {boolean} _embedded - Return embedded resources in response
-    #
-    # Responses:
-    # *  200 - Updated notebook information (https://api.losant.com/#/definitions/notebook)
-    #
-    # Errors:
-    # *  400 - Error if malformed request (https://api.losant.com/#/definitions/error)
-    # *  404 - Error if notebook was not found (https://api.losant.com/#/definitions/error)
-    def upload(params = {})
-      params = Utils.symbolize_hash_keys(params)
-      query_params = { _actions: false, _links: true, _embedded: true }
-      headers = {}
-      body = nil
-
-      raise ArgumentError.new("applicationId is required") unless params.has_key?(:applicationId)
-      raise ArgumentError.new("notebookId is required") unless params.has_key?(:notebookId)
-      raise ArgumentError.new("jupyterFile is required") unless params.has_key?(:jupyterFile)
-
-      headers[:losantdomain] = params[:losantdomain] if params.has_key?(:losantdomain)
-      query_params[:_actions] = params[:_actions] if params.has_key?(:_actions)
-      query_params[:_links] = params[:_links] if params.has_key?(:_links)
-      query_params[:_embedded] = params[:_embedded] if params.has_key?(:_embedded)
-
-      path = "/applications/#{params[:applicationId]}/notebooks/#{params[:notebookId]}/upload"
-
-      @client.request(
-        method: :post,
-        path: path,
-        query: query_params,
-        headers: headers,
-        body: body)
-    end
-
   end
 end
