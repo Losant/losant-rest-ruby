458cdc1051d67312436d3d6fa4755d515c447e74
eeerin <erin@losant.com>
2020-10-21 15:20:52 -0400

    regen client for platform version v1.18.0 and bumping version to v1.11.0

--- lib/losant_rest/client.rb
+++ lib/losant_rest/client.rb
@@ -27,7 +27,7 @@ module LosantRest
   #
   # User API for accessing Losant data
   #
-  # Built For Version 1.17.4
+  # Built For Version 1.18.0
   class Client
     attr_accessor :auth_token, :url
 
@@ -282,7 +282,7 @@ module LosantRest
 
       headers["Accept"]         = "application/json"
       headers["Content-Type"]   = "application/json"
-      headers["Accept-Version"] = "^1.17.4"
+      headers["Accept-Version"] = "^1.18.0"
       headers["Authorization"]  = "Bearer #{self.auth_token}" if self.auth_token
       path = self.url + options.fetch(:path, "")
 
--- lib/losant_rest/version.rb
+++ lib/losant_rest/version.rb
@@ -21,5 +21,5 @@
 # SOFTWARE.
 
 module LosantRest
-  VERSION = "1.10.4"
+  VERSION = "1.11.0"
 end
