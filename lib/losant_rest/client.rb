a1705992695212b69bc6d44139fb11a5f5957666
Erin Thompson <erin@losant.com>
2021-12-10 10:45:19 -0500

    regenerating client for api v1.21.1 and bumping version

--- lib/losant_rest/client.rb
+++ lib/losant_rest/client.rb
@@ -27,7 +27,7 @@ module LosantRest
   #
   # User API for accessing Losant data
   #
-  # Built For Version 1.22.0
+  # Built For Version 1.22.1
   class Client
     attr_accessor :auth_token, :url
 
@@ -358,7 +358,7 @@ module LosantRest
 
       headers["Accept"]         = "application/json"
       headers["Content-Type"]   = "application/json"
-      headers["Accept-Version"] = "^1.22.0"
+      headers["Accept-Version"] = "^1.22.1"
       headers["Authorization"]  = "Bearer #{self.auth_token}" if self.auth_token
       path = self.url + options.fetch(:path, "")
 
--- lib/losant_rest/version.rb
+++ lib/losant_rest/version.rb
@@ -21,5 +21,5 @@
 # SOFTWARE.
 
 module LosantRest
-  VERSION = "1.15.0"
+  VERSION = "1.15.1"
 end
