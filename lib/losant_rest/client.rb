5024ff2009e05731220531a19570a79063f8beb1
eeerin <thompsonerin42@gmail.com>
2023-03-30 14:03:39 -0400

    rebuilding api v1.24.3

--- lib/losant_rest/client.rb
+++ lib/losant_rest/client.rb
@@ -27,7 +27,7 @@ module LosantRest
   #
   # User API for accessing Losant data
   #
-  # Built For Version 1.24.1
+  # Built For Version 1.24.3
   class Client
     attr_accessor :auth_token, :url
 
@@ -382,7 +382,7 @@ module LosantRest
 
       headers["Accept"]         = "application/json"
       headers["Content-Type"]   = "application/json"
-      headers["Accept-Version"] = "^1.24.1"
+      headers["Accept-Version"] = "^1.24.3"
       headers["Authorization"]  = "Bearer #{self.auth_token}" if self.auth_token
       path = self.url + options.fetch(:path, "")
 
--- lib/losant_rest/version.rb
+++ lib/losant_rest/version.rb
@@ -21,5 +21,5 @@
 # SOFTWARE.
 
 module LosantRest
-  VERSION = "1.17.2"
+  VERSION = "1.17.3"
 end
